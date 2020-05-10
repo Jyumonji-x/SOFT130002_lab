2be9866e94ab5303ef8980f1ee66ceed932e02bc
xtq2000 <19302010015@fudan.edu.cn>
2020-05-10 21:51:31 +0800

    Add files via upload

---
+++ lab7/lab07.js
@@ -0,0 +1,56 @@
+const works = [
+    { author: "Micheal Jackson",lifetime:"1022-1055",tips: "Human", photos: ["human1.jpg","human2.jpg","human3.jpg"] },
+    { author: "Maria JK",lifetime:"1920-2001", tips: "Classical", photos: ["classical1.jpg","classical2.jpg"] },
+    { author: "John Herry UY", lifetime:"1894-1928",tips: "Abstract", photos: ["abstract1.jpg","abstract2.jpg","abstract3.jpg","abstract4.jpg","abstract5.jpg"] },
+    { author: "Coco",lifetime:"1777-1799", tips: "Beauty",  photos: ["beauty1.jpg","beauty2.jpg"] }
+];
+let container = document.getElementsByClassName("flex-container");
+for(let i =0;i<works.length;i++){
+    let item = document.createElement("div");
+    item.classList.add("item");
+
+    let genre = document.createElement("h4");
+    genre.innerHTML="Genre:"+works[i].tips;
+
+
+    let authorInnerBox = document.createElement("div");
+    authorInnerBox.classList.add("inner-box");
+
+    let author = document.createElement("h3");
+    author.style.display="inline";
+    author.innerHTML = works[i].author;
+
+    let lifetime = document.createElement("div");
+    lifetime.style.display="inline";
+    lifetime.style.fontWeight="bold";
+    lifetime.innerHTML = "&nbsp;&nbsp;lifetime:"+ works[i].lifetime;
+    authorInnerBox.append(author,lifetime);
+
+
+
+
+    let photoInnerBox = document.createElement("div");
+    photoInnerBox.classList.add("inner-box");
+
+    let popularPhotos = document.createElement("h3");
+    popularPhotos.innerHTML = "Popular Photos";
+    photoInnerBox.append(popularPhotos);
+
+    for(let j=0;j<works[i].photos.length;j++){
+        let photo = document.createElement("img");
+        photo.classList.add("photo");
+        photo.src = works[i].photos[j];
+        photoInnerBox.append(photo);
+    }
+
+
+
+
+    let button = document.createElement("button");
+    button.innerHTML="Visit";
+
+    item.append(genre,authorInnerBox,photoInnerBox,button);
+    container[0].append(item);
+}
+
+
---
+++ lab7/readme.md
@@ -0,0 +1,19 @@
+# Lab07设计文档
+
+许同樵 19302010015
+
+## 遇到的困难与解决方案
+
+1、天才的我甚至一开始全部用document.write写了一遍，竟然还实现了。但是代码实在太恶心，仔细读了一遍w3school后发现不是这么回事……
+
+2、相关命令
+
+按类寻找元素： getElementsByClassName()
+
+创建元素： createElement(TagName)
+
+载入元素： append(Node)
+
+实现标题占用同一行的css： display：inline
+
+3、使用由getElementsByClassName()获取的变量时，添加元素需用角标的方式确定到指定元素
