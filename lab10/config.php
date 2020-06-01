e9b3c0e8744ce404f2ae371e412f05cd13ffffb7
xtq2000 <19302010015@fudan.edu.cn>
2020-06-02 00:53:12 +0800

    Add files via upload

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'lab10_art');
+define('DBUSER', 'lab10');
+define('DBPASS', '123456');
+define('DBCONNSTRING','mysql:dbname=lab10_art;charset=utf8mb4;');
+?>
